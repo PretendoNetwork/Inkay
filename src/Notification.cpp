a0fc5c5c967dd41047ed5228c420ea84ad5b9eec
Jemma <Jemma@jemsoftware.dev>
2023-01-29 12:24:44 -0600

    Revert "Implemented atomics for the thread terminate flag"
    This reverts commit 45f70ffd4e65410d773bba8a069be6502f8c1975.

--- src/Notification.cpp
+++ src/Notification.cpp
@@ -2,21 +2,20 @@
 #include <coreinit/cache.h>
 #include <coreinit/thread.h>
 #include <coreinit/title.h>
-#include <coreinit/atomic.h>
 #include <notifications/notification_defines.h>
 #include <notifications/notifications.h>
 #include <thread>
 
 std::unique_ptr<std::thread> sShowHintThread;
-static uint32_t sShutdownNotificationThread = 0;
+static bool sShutdownHintThread = false;
 static char * notification;
 
 void ShowNotification() {
     // Wait for notification module to be ready
     bool isOverlayReady = false;
-    while (!OSCompareAndSwapAtomic(&sShutdownNotificationThread, 1, 0) && NotificationModule_IsOverlayReady(&isOverlayReady) == NOTIFICATION_MODULE_RESULT_SUCCESS && !isOverlayReady)
+    while (!sShutdownHintThread && NotificationModule_IsOverlayReady(&isOverlayReady) == NOTIFICATION_MODULE_RESULT_SUCCESS && !isOverlayReady)
         OSSleepTicks(OSMillisecondsToTicks(16));
-    if (OSCompareAndSwapAtomic(&sShutdownNotificationThread, 1, 0) || !isOverlayReady) return;
+    if (sShutdownHintThread || !isOverlayReady) return;
     NotificationModuleStatus err = NotificationModule_SetDefaultValue(NOTIFICATION_MODULE_NOTIFICATION_TYPE_INFO, NOTIFICATION_MODULE_DEFAULT_OPTION_DURATION_BEFORE_FADE_OUT, 15.0f);
     if(err != NOTIFICATION_MODULE_RESULT_SUCCESS) return;
 
@@ -26,7 +25,7 @@ void ShowNotification() {
 void StartNotificationThread(char * value) {
     uint64_t titleID = OSGetTitleID();
     if (titleID == 0x0005001010040000L || titleID == 0x0005001010040100L || titleID == 0x0005001010040200L) {
-        OSCompareAndSwapAtomic(&sShutdownNotificationThread, 0, 0);
+        sShutdownHintThread = false;
         notification = value;
         sShowHintThread = std::make_unique<std::thread>(ShowNotification);
     }
@@ -34,7 +33,8 @@ void StartNotificationThread(char * value) {
 
 void StopNotificationThread() {
     if (sShowHintThread != nullptr) {
-        OSCompareAndSwapAtomic(&sShutdownNotificationThread, 0, 1);
+        sShutdownHintThread = true;
+        OSMemoryBarrier();
         sShowHintThread->join();
         sShowHintThread.reset();
     }
