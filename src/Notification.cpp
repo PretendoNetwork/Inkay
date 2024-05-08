d43faeefdf283bb48fd8122b3b220ffc099fd353
Pancake-Pal-Productions <atw3710@gmail.com>
2024-05-08 15:19:27 -0400

    Fix notifications (yay) (thanks maschell)

--- src/Notification.cpp
+++ src/Notification.cpp
@@ -19,11 +19,12 @@
 #include <notifications/notifications.h>
 
 void ShowNotification(std::string_view notification) {
-    auto err1 = NotificationModule_SetDefaultValue(NOTIFICATION_MODULE_NOTIFICATION_TYPE_ERROR,
+    auto err1 = NotificationModule_SetDefaultValue(NOTIFICATION_MODULE_NOTIFICATION_TYPE_INFO,
                                                    NOTIFICATION_MODULE_DEFAULT_OPTION_KEEP_UNTIL_SHOWN, true);
     auto err2 = NotificationModule_SetDefaultValue(NOTIFICATION_MODULE_NOTIFICATION_TYPE_INFO,
                                                    NOTIFICATION_MODULE_DEFAULT_OPTION_DURATION_BEFORE_FADE_OUT,
                                                    15.0f);
+												   
     if (err1 != NOTIFICATION_MODULE_RESULT_SUCCESS || err2 != NOTIFICATION_MODULE_RESULT_SUCCESS) return;
 
     NotificationModule_AddInfoNotification(notification.data());
