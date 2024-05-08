f8bc367e92b60fd23d74b6ba462fc5e2ce74b776
Pancake-Pal-Productions <atw3710@gmail.com>
2024-05-08 15:23:22 -0400

    Fix compiler warnings

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -28,7 +28,7 @@ extern "C" {
 #ifdef DEBUG
 #define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...) DEBUG_FUNCTION_LINE(FMT, ##ARGS)
 #else
-#define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...) while (0)
+#define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...) while (0);
 #endif
 
 #ifdef __cplusplus
