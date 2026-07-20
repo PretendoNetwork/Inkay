82cca21ff354280a2ec87a1859930348eda83b12
Daniel López Guimaraes <danielectra@outlook.com>
2026-07-20 14:40:35 +0100

    chore: Bump version to v3.1.0

--- plugin/src/main.cpp
+++ plugin/src/main.cpp
@@ -23,7 +23,7 @@
 #include "config.h"
 #include "module.h"
 
-#define INKAY_VERSION "v3.0.0"
+#define INKAY_VERSION "v3.1.0"
 
 /**
     Mandatory plugin information.
--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@
 
 #include "ca_pem.h"
 
-#define INKAY_VERSION "v3.0.0"
+#define INKAY_VERSION "v3.1.0"
 
 /**
     Mandatory plugin information.
